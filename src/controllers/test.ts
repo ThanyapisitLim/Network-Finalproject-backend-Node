498b529de386ba25ce73e41c51294dfc58c2213c
Thanyapisit <thanyapisit.lim@gmail.com>
2026-03-19 15:06:14 +0700

    its ok

---
+++ src/controllers/test.ts
@@ -0,0 +1,9 @@
+import supabase from "../config/supabase.js";
+
+async function testFetch(): Promise<any> {
+    const { data, error } = await supabase
+        .from('users')
+        .select('*');
+    if (error) throw error;
+    return data;
+}
